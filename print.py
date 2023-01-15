7e13bb35fefea7d7a54c085dbae2a845ae3989e2
Ryustem Shaban <rs223fx@student.lnu.se>
2023-01-16 00:55:16 +0100

    Assignment exercise

---
+++ print.py
@@ -0,0 +1,5 @@
+lst = ["Knowledge is power!", "Knowledge", "   is    ", "  power!  ", "==========================" ,"|                        |", "|  Knowledge is power!   |", "|                        |", "=========================="]
+counter = 0
+while len(lst) != counter:
+    print(lst[counter])
+    counter += 1
