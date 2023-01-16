0525257cb868a07472e062fd0f6f2e6572a96a8d
Ryustem Shaban <rs223fx@student.lnu.se>
2023-01-16 01:04:17 +0100

    Assignment task

---
+++ largest.py
@@ -0,0 +1,24 @@
+#Write a program randomsum.py generating and printing the sum of five random numbers in the interval [1,100]. For example
+#    Five random numbers: 78 13 91 2 36
+    
+ #   The sum is 222
+#Hint: Use the function random.randint in the random module
+#Notice: No reading from the keyboard in this exercise
+
+a = 1
+b = 100
+n1 = 78
+n2 = 13
+n3 = 91
+n4 = 2
+n5 = 36
+sum = n1 + n2 + n3 + n4 + n5
+
+import random
+
+(random.randint(a , b))
+if  sum:
+    sum = 222
+    print("The sum is " , sum)
+else:
+    print()
