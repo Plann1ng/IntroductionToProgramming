555e8881a9c3cffef30fd7c201d4f037a5db45d5
Ryustem Shaban <rstmduran@gmail.com>
2023-01-18 23:58:45 +0100

    Process on Deque

--- Assignment3/Deque.py
+++ Assignment3/Deque.py
@@ -27,3 +27,37 @@ class Deque:
             new.nxt = self.head
             self.head = new
             self.size += 1
+
+   # Returns a string representation of the current deque content
+    def to_string(self):
+        s = "{ "
+        node = self.head
+        while node is not None:
+            s += str(node.value) + " "
+            node = node.nxt
+        s += "}"
+        return s
+
+    # Add element n as last entry in deque
+    def add_last(self, n):
+        new = Node(n, None)
+        if self.head is None:
+            self.head = new
+            self.size = 1
+            self.tail = self.head
+        else:
+            self.tail.nxt = new
+            self.tail = self.tail.nxt
+            self.size += 1
+
+    # Returns (without removing) the last entry in the deque.
+    # Gives error message and returns None when accessing empty deque.
+    def get_last(self):
+        if self.head is None:
+            print("Get can't be applied on an empty list")
+            return None
+        else:
+            node = self.head
+            for i in range(self.size-1):
+                node = node.nxt
+            return node.value
