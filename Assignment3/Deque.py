1a0374e490c568e71e811e55e9aa411d13fa6a30
Ryustem Shaban <rstmduran@gmail.com>
2023-01-18 23:57:56 +0100

    Deque class process

--- Assignment3/Deque.py
+++ Assignment3/Deque.py
@@ -9,3 +9,21 @@ from typing import Any
 class Node:
     value: int = None
     nxt: Any = None  # Any since Node not properly defined at this point
+
+@dataclass
+class Deque:
+    head: Node = None      # First node in queue
+    tail: Node = None      # Last node in queue
+    size: int = 0
+
+    # Add element n as first entry in deque
+    def add_first(self, n):
+        new = Node(n, None)
+        if self.head is None:
+            self.head = new
+            self.size = 1
+            self.tail = self.head
+        else:
+            new.nxt = self.head
+            self.head = new
+            self.size += 1
