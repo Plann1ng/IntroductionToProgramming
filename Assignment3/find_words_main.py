1be0039d75d074aff500ab3e526a7d7539bce7e4
Ryustem Shaban <rstmduran@gmail.com>
2023-01-19 00:06:24 +0100

    Main program to test file reader

---
+++ Assignment3/find_words_main.py
@@ -0,0 +1,11 @@
+from find_words_functions import read_file_main, get_words_main
+from find_words_functions import save_words_main
+
+# Broke path to 2 pieces, because of the flake8 errors.
+frst = r'C:\Users\Admin\OneDrive - student.lnu.se'
+adittional = r"\Desktop\Holy text\large_texts"
+file = r"\eng_news_100K-sentences.txt"
+path = frst + adittional
+to_file = path + file
+
+outpath = "Outpath.txt"
