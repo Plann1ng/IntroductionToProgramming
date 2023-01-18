9a13fc7aed3bdbe2e862fb988fe5998e6b75abbe
Ryustem Shaban <rstmduran@gmail.com>
2023-01-19 00:04:22 +0100

    Filtered word retriever added

--- Assignment3/find_words_functions.py
+++ Assignment3/find_words_functions.py
@@ -16,3 +16,21 @@ def read_file_main(path):
 # append the list if fulfills the conditions.
 # 的演講。this appears in the end of  the text and no matter what i do i cant get
 # get rid of it so I strippen it as well
+
+def get_words_main(lines):
+    check = ''
+    clean = []
+    allowed = "q w e r t y u i o p a s d f g h j k l z x c v b n m ' -"
+    for line in lines:
+        line = ''.join(line).split()
+        for word in line:
+            word = word.strip("1 2 3 4 5 6 7 8 9 0-!'?.$|( ) ;&*#:[], ” 的演講。")
+            word = word.replace("  ", "")
+            word = word.lower()
+
+            if len(word) > 0:
+                check = word[0]
+            if check in allowed:
+                if len(word) > 0:
+                    clean += [word]
+    return clean
