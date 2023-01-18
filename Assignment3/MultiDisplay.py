60c8bb671e94e1420d5d7f7b0f158ff0112f754d
Ryustem Shaban <rstmduran@gmail.com>
2023-01-19 00:10:22 +0100

    Task process

--- Assignment3/MultiDisplay.py
+++ Assignment3/MultiDisplay.py
@@ -11,3 +11,22 @@ class MultiDisplay:
     # Function which will take integer part of the class
     def set_count(self, number):
         self.count = number
+    # Returns the string provided
+    def get_message(self):
+        return self.message
+
+    # Returns the integer provided
+    def get_count(self):
+        return self.count
+
+    # Returns the string "x" times while everytime it
+    # being printed onto a new line
+    def display(self):
+        return print(('\n' + self.message) * self.count)
+
+    # Takes string and integer and returns "x" times the string.
+    # Each time will be printed onto a new line
+    def set_display(self, word, number):
+        self.message = word
+        self.count = number
+        return print(('\n'+word) * number)
