569e49d4ed5f0ea22607dc813fd068aaa49e332b
Ryustem Shaban <rstmduran@gmail.com>
2023-01-19 00:01:52 +0100

    Deque test finalized

--- Assignment3/deque_main.py
+++ Assignment3/deque_main.py
@@ -27,3 +27,21 @@ print("get_first() on empty deque:", empty.get_first())
 # Test remove_first
 print("\nremove_first():", deque.remove_first())
 print("remove_first() on empty deque:", empty.remove_first())
+
+# Test remove_last
+print("\nremove_last():", deque.remove_last())
+print("remove_last() on empty deque:", empty.remove_last())
+
+print(deque.to_string())
+print("Size:", deque.size)
+
+# Test add and remove all
+print("\nTest to remove all elements")
+temp = deq.Deque()
+for i in range(100, 106):
+    temp.add_first(i)
+print("After adding elements:", temp.to_string())
+while temp.size > 0:
+    temp.remove_last()
+print("After removing all elements:", temp.to_string())
+print("Size:", temp.size)
