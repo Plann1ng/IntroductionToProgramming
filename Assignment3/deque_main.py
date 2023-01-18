26a0b581fe08f1370dd95551d45fc7409edf25a6
Ryustem Shaban <rstmduran@gmail.com>
2023-01-19 00:01:17 +0100

    Deque test

--- Assignment3/deque_main.py
+++ Assignment3/deque_main.py
@@ -15,3 +15,15 @@ for i in range(11, 21):
     deque.add_first(i)
 print(deque.to_string())
 print("Size:", deque.size)
+
+# Test get_last
+print("\nget_last():", deque.get_last())
+print("get_last() on empty deque:", empty.get_last())
+
+# Test get_first
+print("\nget_first():", deque.get_first())
+print("get_first() on empty deque:", empty.get_first())
+
+# Test remove_first
+print("\nremove_first():", deque.remove_first())
+print("remove_first() on empty deque:", empty.remove_first())
