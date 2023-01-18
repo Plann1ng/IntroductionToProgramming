f287536fa4a8a5595a8c9633ac81d21f955fe732
Ryustem Shaban <rstmduran@gmail.com>
2023-01-18 23:57:12 +0100

    Started with Deque class

---
+++ Assignment3/Deque.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from typing import Any
+
+# A head-and-tail implementation of a deque using data classes
+
+
+# Each node is an instance of class Node
+@dataclass
+class Node:
+    value: int = None
+    nxt: Any = None  # Any since Node not properly defined at this point
