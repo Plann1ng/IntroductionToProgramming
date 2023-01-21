43c0a6a0928299d6594d004e90a18d6bb85c51bf
Ryustem Shaban <rstmduran@gmail.com>
2023-01-21 03:09:16 +0100

    Top 10 most occurying word counter method added

--- Python_mini_project/part_two.py
+++ Python_mini_project/part_two.py
@@ -25,3 +25,26 @@ def count_unique(word_list):
         hashTwo.add(self_hsh, element)
     print("The number of unique words is: ", self_hsh.get_size())
 
+### 3.2 ten most frequent words using the binary search tree
+def top10(word_list):
+    self_bst.clear_all()
+    bst = bstTwo.BstMap()
+    result = ''
+    for word in word_list:
+        if len(word) < 5:
+            continue
+        value = bst.get(word)
+        if value is not None:
+            nc = value + 1
+            bst.put(word, nc)
+        else:
+            bst.put(word, 1)
+    bst = sorted(bst.as_list(), key=lambda tpl: -tpl[1])
+    print("10 most frequent used words:\n")
+    for i in range(0, 10):
+        result += f'"{bst[i][0]}"\tis used: {bst[i][1]} times.\n'
+    print(result)
+
+
+
+
