f70a3a7542d1612c8c6a911b84d589ea20fdfad0
Ryustem Shaban <rstmduran@gmail.com>
2023-01-21 02:57:29 +0100

    adder and to string method added

--- Python_mini_project/HashSet.py
+++ Python_mini_project/HashSet.py
@@ -32,3 +32,22 @@ class HashSet:
             self.buckets.append([])     #append new empty buckets
         for element in copy_buckets:          #copy all previous elements in new sized set
             self.add(element)
+
+    # Adds a word to set if not already added
+    def add(self, word):
+        if not self.contains(word):
+            hashval = self.get_hash(word)
+            index = hashval % len(self.buckets)
+            self.buckets[index].append(word)
+            self.size += 1
+        if self.bucket_list_size() == self.get_size():
+            self.rehash()
+
+    # Returns a string representation of the set content
+    def to_string(self):
+        str = "{ "
+        for i in self.buckets:
+            for element in i:
+                str += element + " "
+        str += " }"
+        return str
